ebde7bbf3b831393ba35ffaeb3af8de52fcdd01d
sungminyang <lonesome661@naver.com>
2025-03-02 17:35:57 +0900

    Feat: proejct init

---
+++ app/src/test/kotlin/com/tars/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tars.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
