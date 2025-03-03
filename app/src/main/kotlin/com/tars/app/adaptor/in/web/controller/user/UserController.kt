13254ce306b97cb2b4bf2865430930809dcc1850
sungminyang <lonesome661@naver.com>
2025-03-04 01:01:45 +0900

    Feat : 주석 추가

--- app/src/main/kotlin/com/tars/app/adaptor/in/web/controller/user/UserController.kt
+++ app/src/main/kotlin/com/tars/app/adaptor/in/web/controller/user/UserController.kt
@@ -2,6 +2,7 @@ package com.tars.app.adaptor.`in`.web.controller.user
 
 import com.tars.app.adaptor.`in`.web.controller.user.dto.UserRegistrationDto
 import com.tars.app.application.user.UserRegistrationUseCase
+import com.tars.app.application.user.service.UserRegistrationService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
@@ -15,10 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("v1/user")
 class UserController(
-    private val userRegistrationUseCase: UserRegistrationUseCase
+    private val userRegistrationService: UserRegistrationService
 ) {
-
-    //TODO : 현재는 request 를 풀어놓았지만 필요한 정보만 fit 하게 받도록 수정할것.
 
     @Operation(
         summary = "사용자 등록",
@@ -31,7 +30,8 @@ class UserController(
     )
     @PostMapping
     fun registerUser(@RequestBody req: UserRegistrationDto): ResponseEntity<UserRegistrationUseCase.Response> {
-        val response = userRegistrationUseCase.registerUser(
+        //TODO : service input 수정 할것.
+        val response = userRegistrationService.registerUser(
             email = req.email,
             rawPassword = req.password,
             ssn = req.ssn,
