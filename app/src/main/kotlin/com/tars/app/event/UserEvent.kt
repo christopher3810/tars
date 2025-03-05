f66531a0e4b0f670ab25ab83f55dd21c4cbe6b55
sungminyang <lonesome661@naver.com>
2025-03-05 23:29:24 +0900

    Feat : domain Event define

--- app/src/main/kotlin/com/tars/app/event/UserEvent.kt
+++ app/src/main/kotlin/com/tars/app/event/UserEvent.kt
@@ -1,4 +1,18 @@
 package com.tars.app.event
 
-class UserEvent {
-}
+sealed class UserEvent : DomainEvent {
+    data class UserCreated(
+        val email: String,
+        val name: String
+    ) : UserEvent()
+
+    data class AddressChanged(
+        val email: String,
+        val newAddress: String?
+    ) : UserEvent()
+
+    data class RoleAdded(
+        val email: String,
+        val role: String
+    ) : UserEvent()
+} 
