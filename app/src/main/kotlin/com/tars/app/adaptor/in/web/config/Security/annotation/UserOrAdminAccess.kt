96778574fcbf8dc6590b0cea62d8bbe4d70c1bfb
sungminyang <lonesome661@naver.com>
2025-03-07 08:02:35 +0900

    Feat : auth module 활용을 위해 app 에서 제거

--- app/src/main/kotlin/com/tars/app/adaptor/in/web/config/Security/annotation/UserOrAdminAccess.kt
+++
@@ -1,9 +0,0 @@
-package com.tars.app.adaptor.`in`.web.config.Security.annotation
-
-import org.springframework.security.access.prepost.PreAuthorize
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("hasAnyRole('USER','ADMIN')")
-annotation class UserOrAdminAccess {
-}
