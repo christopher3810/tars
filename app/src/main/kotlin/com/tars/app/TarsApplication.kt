e6a86c3ce7dec049250b6a28d5fb53734ee97f81
sungminyang <lonesome661@naver.com>
2025-03-07 16:15:44 +0900

    Faet : app 모듈 컴포넌트스캔 변경

--- app/src/main/kotlin/com/tars/app/TarsApplication.kt
+++ app/src/main/kotlin/com/tars/app/TarsApplication.kt
@@ -1,11 +1,18 @@
 package com.tars.app
 
+import com.tars.auth.AuthModuleConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Import
 
-@SpringBootApplication
+/**
+ * 애플리케이션의 진입점
+ * auth-module의 설정을 가져와서 통합합니다.
+ */
+@SpringBootApplication(scanBasePackages = ["com.tars.app"])
 @EnableFeignClients
+@Import(AuthModuleConfig::class)
 class AppApplication
 
 fun main(args: Array<String>) {
