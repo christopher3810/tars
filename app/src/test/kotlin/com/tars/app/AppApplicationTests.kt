3b05a5f0530331dd478645872683293b40641aa3
sungminyang <lonesome661@naver.com>
2025-03-07 16:15:21 +0900

    Feat : app 모듈 application yml 추가

--- app/src/test/kotlin/com/tars/app/AppApplicationTests.kt
+++ app/src/test/kotlin/com/tars/app/AppApplicationTests.kt
@@ -3,11 +3,15 @@ package com.tars.app
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * 애플리케이션 컨텍스트 로드 테스트
+ */
 @SpringBootTest
 class AppApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		// 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인
 	}
 
 }
